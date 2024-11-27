9cbdd1a6d1094516a1e42efaf164762e0d0a78b8
roxanatera <jrnt17@gmail.com>
2024-11-27 19:40:22 +0100

    corriegiendo archivo axios

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Verifica que esto esté correcto
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // Verifica que esto esté correcto
 });
 
 // Explicitly set JSON as the content type
