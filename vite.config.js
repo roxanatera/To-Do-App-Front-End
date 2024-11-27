938d587fea7569464cb8d2a93d02b3101b79d69b
roxanatera <jrnt17@gmail.com>
2024-11-27 18:18:41 +0100

    Agregando Api

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000", // Enlace al back-end
-    },
+  build: {
+    outDir: "dist", // Carpeta de salida para los archivos de producción
   },
 });
