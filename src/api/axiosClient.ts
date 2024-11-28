fa14d4f1e9c1da9e2ae79d1770f854db22b71d20
roxanatera <jrnt17@gmail.com>
2024-11-28 13:19:18 +0100

    agregando _redirects

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
+// Asumiendo que tienes una variable de entorno llamada VITE_API_URL configurada en tus archivos .env
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 });
 
 // Explicitly set JSON as the content type
 axiosClient.defaults.headers.common['Content-Type'] = 'application/json';
-
 
 // Interceptor to handle authentication errors
 axiosClient.interceptors.response.use(response => response, error => {
